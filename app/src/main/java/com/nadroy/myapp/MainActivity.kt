c920e162070717bc55e9382558b52f419f9f0f12
Yordan Tarazona <yordantarazona9@gmail.com>
2022-10-25 20:46:47 -0500

    Se creo el login normal, falta implementar el soporte de idioma en el login

--- app/src/main/java/com/nadroy/myapp/MainActivity.kt
+++ app/src/main/java/com/nadroy/myapp/MainActivity.kt
@@ -1,19 +1,44 @@
 package com.nadroy.myapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private var edit_user: EditText?=null
+    private var edit_password: EditText?=null
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edit_user = findViewById(R.id.edit_user)
+        edit_password = findViewById(R.id.edit_password)
     }
 
 
     fun ingresar(btningresar: View){
-        val ingreso = Intent(this,Welcome::class.java)
-        startActivity(ingreso)
+
+        if (edit_user!!.text.toString()=="admin"){
+            if (edit_password!!.text.toString()=="admin"){
+                val ingreso = Intent(this,Welcome::class.java)
+                startActivity(ingreso)
+            }
+        }else{
+            val dialogo = AlertDialog.Builder(this)
+                .setTitle("¡Alerta!")
+                .setMessage("Usuario o Contraseña incorrectos...") // R.string.variable
+                .create().show()
+        }
+
+
+
+
     }
 }
