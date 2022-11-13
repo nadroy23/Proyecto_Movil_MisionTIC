94996ee4604943d0061152e0486a64cbecf90ca4
Yordan Tarazona <yordantarazona9@gmail.com>
2022-11-13 10:23:34 -0500

    se agrego menu

--- app/src/main/java/com/nadroy/myapp/MainActivity.kt
+++ app/src/main/java/com/nadroy/myapp/MainActivity.kt
@@ -4,8 +4,11 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
@@ -36,9 +39,55 @@ class MainActivity : AppCompatActivity() {
                 .setMessage("Usuario o Contraseña incorrectos...") // R.string.variable
                 .create().show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_barra, menu)
+
+        return super.onCreateOptionsMenu(menu)
 
 
+    }
+
+    fun OnOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.itm_buscar -> {
+            Toast.makeText(this, "seleccion de buscar ", Toast.LENGTH_SHORT).show()
+            true
+        }
+        R.id.itm_ayuda -> {
+            Toast.makeText(this, "seleccion ayuda ", Toast.LENGTH_SHORT).show()
+            true
+        }
+        R.id.itm_direccion -> {
+            Toast.makeText(this, "seleccion de direccion ", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        R.id.itm_subir -> {
+            val intento = Intent(this, Welcome::class.java)
+            startActivity(intento)
+            Toast.makeText(this, "seleccion subir ", Toast.LENGTH_SHORT).show()
+            true
+        }
+        R.id.itm_editar -> {
 
 
+            Toast.makeText(this, "seleccion editar ", Toast.LENGTH_SHORT).show()
+
+
+            true
+        }
+        R.id.itm_llamar -> {
+
+
+            Toast.makeText(this, "seleccion de llamar ", Toast.LENGTH_SHORT).show()
+
+
+            true
+
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
